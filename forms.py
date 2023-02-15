c55eb86ae034d935dd927b417dba64d29bc0a70d
Aybarinee Campbell Mendez <Aybarineecampbellmendez@gmail.com>
2023-02-15 06:38:14 -0500

    Done up to question 3 part 3

---
+++ forms.py
@@ -0,0 +1,11 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Email
+
+
+class ContactForm():
+    name= StringField("Name", Validators= [DataRequired()], description= " Please enter your Fullname")
+    email= StringField("E-emai;", validators= [DataRequired(), Email()], description= "Please enter your Emaill address")
+    subject: StringField( "Subject", validators= [DataRequired()], description= "Please enter your Subject")
+    message= TextAreaField('Message', validators=[DataRequired], description= "Please enter your Message") 
+
